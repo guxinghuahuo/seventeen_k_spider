032f7185d6a3c8c7e6e91af8f83ffc2d187c45bb
hhx <guxinghuahuo@aliyun.com>
2024-12-26 17:11:39 +0800

    爬取各章节内容

--- seventeen_k/spiders/novel.py
+++ seventeen_k/spiders/novel.py
@@ -3,8 +3,10 @@ from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import Spider
 from scrapy_splash import SplashRequest
 import random
+from seventeen_k.utils import headers
 
 wait_time = random.uniform(3, 5)
+domain = 'https://www.17k.com'
 
 class NovelSpider(Spider):
     name = "novel"
@@ -22,21 +24,16 @@ class NovelSpider(Spider):
 
         link_extractor = LinkExtractor(allow=r'/book/\d+.html', restrict_xpaths=('//td[@class="td3"]/span/a'))
         links = link_extractor.extract_links(response)
-        with open('parse.txt', 'w', encoding='utf-8') as f:
+        with open('books.txt', 'w', encoding='utf-8') as f:
             f.write(response.text)
 
-        print('链接数量：', len(links), links)
+        # print('链接数量：', len(links), links)
         for index, link in enumerate(links):
-            headers = {
-                    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-                    "Accept-Language": "zh-CN,zh;q=0.8,en-US;q=0.5,en;q=0.3",
-                    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-                    'Referer': link.url
-                }
+            header = headers.get_headers(link.url)
             # 限制一本书
             if index == 0:
                 print("限制一本书：", link.url)
-                yield SplashRequest(link.url, self.parse_book, args={'wait': wait_time}, headers=headers)
+                yield SplashRequest(link.url, self.parse_book, args={'wait': wait_time}, headers=header)
             else:
                 return
             
@@ -47,7 +44,7 @@ class NovelSpider(Spider):
         """
         item = {}
         print('解析书籍：', response.url)
-        with open('links.txt', 'w', encoding='utf-8') as f:
+        with open('book.txt', 'w', encoding='utf-8') as f:
             f.write(response.text)
         # 获取书籍信息
         title = response.xpath('//div[@class="Info "]/h1/a/text()').extract_first()
@@ -55,7 +52,7 @@ class NovelSpider(Spider):
         category = response.xpath('//dl[@id="bookInfo"]/dd/div[2]/table/tbody/tr/td[2]/a/text()').extract_first()
         discription = ''.join(response.xpath('//*[@id="bookInfo"]/dd/div[1]/p/a/text()').extract())
         novel_url = response.url
-        chapters_url = response.xpath('//dt[@class="read"]/a/@href').extract_first()
+        chapters_url = domain + response.xpath('//dt[@class="read"]/a/@href').extract_first()
 
         print('title:', title)
         print('word_count:', word_count)
@@ -64,36 +61,59 @@ class NovelSpider(Spider):
         print('novel_url:', novel_url)
         print('chapters_url:', chapters_url)
 
-        # 存储信息
-        # item['title'] = title
-        # item['word_count'] = word_count
-        # item['category'] = category
-        # item['discription'] = discription
-        # item['novel_url'] = novel_url
-        # item['chapters_url'] = chapters_url
+        header = headers.get_headers(chapters_url)
 
-        # 返回抓取的书籍信息
+        yield SplashRequest(chapters_url, self.parse_chapter, args={'wait': wait_time}, headers=header)
+
+
+
+    def parse_chapter(self, response):
+        """
+        解析章节页面
+        """
+        item = {}
+        print('解析章节：', response.url)
+
+        # # 存储信息
+        # item['chapter_title'] = chapter_title
+        # item['chapter_content'] = chapter_content
+        # item['chapter_url'] = response.url
+
+        link_extractor = LinkExtractor(allow=r'/chapter/\d+/\d+.html', restrict_xpaths=('//dl[@class="Volume"]/dd/a'))
+        links = link_extractor.extract_links(response)
+        with open('charpters.txt', 'w', encoding='utf-8') as f:
+            f.write(response.text)
+
+        # print('链接数量：', len(links), links)
+        for index, link in enumerate(links):
+            header = headers.get_headers(link.url)
+            # 限制一个章节
+            if index == 0:
+                print("限制一个章节：", link.url)
+                yield SplashRequest(link.url, self.parse_chapter_content, args={'wait': wait_time}, headers=header)
+            else:
+                return
+
+        
+    def parse_chapter_content(self, response):
+        """
+        解析章节内容页面
+        """
+        item = {}
+        print('解析章节内容：', response.url)
+        with open('charpter_content.txt', 'w', encoding='utf-8') as f:
+            f.write(response.text)
+
+        # # 获取章节内容
+        chapter_title = response.xpath('//div[@class="readAreaBox content"]/h1/text()').extract()
+        chapter_content = '\n'.join(response.xpath('//div[@class="readAreaBox content"]/div[@class="p"]/p/text()').extract())
+
+        # # 存储信息
+        # item['chapter_title'] = chapter_title
+        # item['chapter_content'] = chapter_content
+        # item['chapter_url'] = response.url
+
+        print('chapter_title:', chapter_title)
+        print('chapter_content:', chapter_content)
+
         yield item
-
-    #     # 继续抓取章节页面
-    #     if chapters_url:
-    #         yield SplashRequest(chapters_url, self.parse_chapter, args={'wait': 2})
-
-    # def parse_chapter(self, response):
-    #     """
-    #     解析章节页面
-    #     """
-    #     item = {}
-    #     print('解析章节：', response.url)
-
-    #     # 获取章节内容
-    #     chapter_title = response.xpath('//h1[@class="chapter-title"]/text()').extract_first()
-    #     chapter_content = ''.join(response.xpath('//div[@class="chapter-content"]/p/text()').extract())
-
-    #     # 存储信息
-    #     item['chapter_title'] = chapter_title
-    #     item['chapter_content'] = chapter_content
-    #     item['chapter_url'] = response.url
-
-    #     # 返回抓取的章节信息
-    #     yield item
